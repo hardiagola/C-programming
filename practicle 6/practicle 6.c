b3f6a33144c2b37bf7916ff09db90a51686b26b7
hardiagola <hardi.agola@gmail.com>
2025-07-28 15:30:03 +0530

    Add files via upload

---
+++ practicle 6/practicle 6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    printf("conversion of temperature from Celsius to Fahrenheit\n");
+    float Celsius;
+    printf("enter the temperature in Celsius:\n");
+    scanf("%f",&Celsius);
+    float Fahrenheit= ((Celsius * 9.0)/5.0)+32;
+    printf("temperature in Fahrenheit is:%f\n",Fahrenheit);
+
+}
+
+
+
+
