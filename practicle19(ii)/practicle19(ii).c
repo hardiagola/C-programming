1e29e433308075c44026add1de2921fa7c465fc3
hardiagola <hardi.agola@gmail.com>
2025-09-08 14:23:19 +0530

    Add files via upload

---
+++ practicle19(ii)/practicle19(ii).c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n=9;
+
+    for(i=1 ; i<=(n/2)+1 ; i++)
+    {
+        for(j=1 ; j<=n ; j++)
+        {
+            if (i==1)
+                printf("%d",j);
+            else if (i==j || i+j==n+1)
+                printf("%d",j);
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
